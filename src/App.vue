3c9b17090c3fb122ccb9acd52fc6f59a47287bc4
denhuii <873023670@qq.com>
2020-11-05 02:58:25 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/">Home</router-link> |
-      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/video">Video</router-link>
     </div>
