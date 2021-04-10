3908fb7b5b5970de83547ea7b38b5168f4f8f99f
denhuii <873023670@qq.com>
2021-04-10 22:43:59 +0800

    回滚

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,19 +1,52 @@
 <template>
   <div>
-    <svg-icon class="arrow_right" icon-class="rightArrow" />
+    <!-- <video-player
+      class="video-player-box"
+      ref="videoPlayer"
+      :options="playerOptions"
+      :playsinline="true"
+      customEventName="customstatechangedeventname"
+    >
+    </video-player> -->
+    <video src="https://doc.denhuii.com/api/file/video.mp4" controls="controls">
+      <!-- http://116.63.145.106:8888/file/video.mp4 -->
+      您的浏览器不支持 video 标签。
+    </video>
   </div>
 </template>
 
 <script>
+// import { videoPlayer } from 'vue-video-player';
+import 'video.js/dist/video-js.css';
 export default {
   data() {
-    return {};
+    return {
+      playerOptions: {
+        // videojs options
+        muted: true,
+        language: 'en',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [
+          {
+            type: 'video/mp4',
+            src: 'http://116.63.145.106:8888/file/video.mp4',
+          },
+        ],
+        poster: '/static/images/author.jpg',
+      },
+    };
   },
-
-  watch: {},
-  mounted() {},
-  computed: {},
-  components: {},
+  mounted() {
+    console.log('this is current player instance object', this.player);
+  },
+  computed: {
+    player() {
+      return this.$refs.videoPlayer.player;
+    },
+  },
+  components: {
+    // videoPlayer,
+  },
 };
 </script>
 
