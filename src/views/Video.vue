ed89e2616f4be04263d9582937de770e37a5a087
denhuii <873023670@qq.com>
2021-04-10 22:29:29 +0800

    Revert "测试视频"
    This reverts commit 663d156f813c7f139db7775c37d3317c0c41c680.

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,36 +1,52 @@
 <template>
   <div>
-    <video
-      src="https://doc.denhuii.com/api/file/IPX-557.mp4"
-      controls="controls"
-      v-if="isShow"
+    <!-- <video-player
+      class="video-player-box"
+      ref="videoPlayer"
+      :options="playerOptions"
+      :playsinline="true"
+      customEventName="customstatechangedeventname"
     >
+    </video-player> -->
+    <video src="https://doc.denhuii.com/api/file/video.mp4" controls="controls">
+      <!-- http://116.63.145.106:8888/file/video.mp4 -->
       您的浏览器不支持 video 标签。
     </video>
-    <input type="text" v-model="text" />
   </div>
 </template>
 
 <script>
+// import { videoPlayer } from 'vue-video-player';
+import 'video.js/dist/video-js.css';
 export default {
   data() {
     return {
-      text: '',
-      isShow: false,
+      playerOptions: {
+        // videojs options
+        muted: true,
+        language: 'en',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [
+          {
+            type: 'video/mp4',
+            src: 'http://116.63.145.106:8888/file/video.mp4',
+          },
+        ],
+        poster: '/static/images/author.jpg',
+      },
     };
   },
-
-  watch: {
-    text(x) {
-      console.log(x);
-      if (x == '8785452') {
-        this.isShow = true;
-      }
+  mounted() {
+    console.log('this is current player instance object', this.player);
+  },
+  computed: {
+    player() {
+      return this.$refs.videoPlayer.player;
     },
   },
-  mounted() {},
-  computed: {},
-  components: {},
+  components: {
+    // videoPlayer,
+  },
 };
 </script>
 
